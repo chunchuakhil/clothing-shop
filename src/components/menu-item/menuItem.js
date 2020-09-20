75093263342234f20594426e3a23e910b779afc5
akhil chunchu <akhilchunchu52@gmail.com>
2020-09-20 11:01:04 +0530

    initial

---
+++ src/components/menu-item/menuItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./menuItem.scss";
+const MenuItem = (props) => {
+  console.log("MenuItem -> props", props);
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${props.imageUrl})`,
+      }}
+      className="menu-item"
+    >
+      <button
+        onClick={(e) => {
+          props.history.push("/hats");
+        }}
+      >
+        Button
+      </button>
+      <div className="content">
+        <h1 className="title"> {props.title}</h1>
+        <span className="subtitle">shop here</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
